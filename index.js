6be85e973793b1ee7e69bdd14b3b8a4ac25b5189
Ibarra1812 <ibarraalvaro852@gmail.com>
2025-06-10 23:59:31 -0300

    Completed exercise

--- index.js
+++ index.js
@@ -63,6 +63,26 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 }
 )
+const generateId = () => {
+  let id
+  do {
+    id = Math.floor(Math.random() * 1e12)
+  } while (person.some((p) => p.id === id))
+  return id
+}
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  const newPerson = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  person = person.concat(newPerson)
+  response.json(newPerson)
+}
+)
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
