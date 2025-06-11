7f769d09b0575bb459601376634972833057fa63
Ibarra1812 <ibarraalvaro852@gmail.com>
2025-06-11 00:09:36 -0300

    Completed exercise

--- index.js
+++ index.js
@@ -73,6 +73,17 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing',
+    })
+  }
+  if (person.some((p) => p.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique',
+    })
+  }
+
   const newPerson = {
     id: generateId(),
     name: body.name,
