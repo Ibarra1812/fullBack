1a9833b140f7d447d08780535f62abf2191e3f9b
Ibarra1812 <ibarraalvaro852@gmail.com>
2025-06-16 11:33:56 -0300

    Complete exercise

--- index.js
+++ index.js
@@ -33,8 +33,12 @@ let person =  [
     number: "987-654-3210"
   }
 ]
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 
 
 app.get('/', (request, response) => {
